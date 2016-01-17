f90f6e123318eefb31b30b6588214e47d310051e
unknown <ilya.chase>
2016-01-17 16:58:35 +0500

    MazeMatrix - 2d-представление лабиринта, публичные параметры ширины и высоты лабиринта
    Мы будем использовть depth-search для генерации лабиринта, так что в
    mazeMatrix уже проставляются "стенки" и "ячейки"

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class Maze : MonoBehaviour {
+	public int width, height; // Ширина и высота лабиринта
+
+	private int[,] mazeMatrix; // Матрица лабиринта, 
 
 	// При инстанциации, лабиринт должен генирироваться
 	void Start () {
@@ -10,6 +13,15 @@ public class Maze : MonoBehaviour {
 	
 	// Генерация лабиринта
 	void GenerateMaze () {
-		Debug.Log("Maze generated");
+		mazeMatrix = new int[width, height];
+
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if ((i % 2 != 0 && j % 2 != 0) && // Если ячейка нечетная по x и y, 
+				   (i < height - 1 && j < width - 1)) // И при этом находится в пределах стен лабиринта
+					mazeMatrix[i,j] = 0; // То это пустая КЛЕТКА
+				else mazeMatrix[i,j] = 1; // Иначе это СТЕНА
+			}
+		}
 	}
 }

312f9561380f5560c52cd228d9f6931c7ce040aa
unknown <ilya.chase>
2016-01-17 16:12:32 +0500

    Скрипт GameManager, запуск игры по пробелу

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	// Точка вхождения игры
+	void Update () {
+		// По нажатию пробела запускаем игру
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			StartGame();
+		}
+	}
+
+	void StartGame() {
+		Debug.Log("Game started");
+	}
+}

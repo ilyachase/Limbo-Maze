23aeab511332fdc80b832692cac5ed6076a0054d
unknown <ilya.chase>
2016-01-18 23:23:26 +0500

    Revert "2d array -> jugged byte list"
    This reverts commit e14e59e7f6b4865accc9098c05b368d5d7510d81.

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -1,40 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class Maze : MonoBehaviour {
 	public int width, height; // Ширина и высота лабиринта
 
-	private List<List<byte>> mazeMatrix = new List<List<byte>>(); // Матрица лабиринта
+	private int[,] mazeMatrix; // Матрица лабиринта, 
 
-	// При инстанциации, лабиринт должен сгенирироваться
+	// При инстанциации, лабиринт должен генирироваться
 	void Start () {
 		GenerateMaze();
 	}
 	
 	// Генерация лабиринта
 	void GenerateMaze () {
-		// Инициализация матрицы
-		for (int i = 0; i < height; i++)
-			mazeMatrix.Add(new List<byte>(new byte[width]));
+		mazeMatrix = new int[width, height];
 
-		// Начальная расстановка пустых ячеек и стен
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
 				if ((i % 2 != 0 && j % 2 != 0) && // Если ячейка нечетная по x и y, 
 				   (i < height - 1 && j < width - 1)) // И при этом находится в пределах стен лабиринта
-					mazeMatrix[i][j] = 0; // То это пустая КЛЕТКА
-				else mazeMatrix[i][j] = 1; // Иначе это СТЕНА
+					mazeMatrix[i,j] = 0; // То это пустая КЛЕТКА
+				else mazeMatrix[i,j] = 1; // Иначе это СТЕНА
 			}
-		}
-		outp();
+		}outp();
 	}
 
 	void outp() {
 		string o = "";
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++)
-				o += mazeMatrix[i][j] + " ";
+				o += mazeMatrix[i, j] + " ";
 			o += "\n";
 		}
 		Debug.Log(o);
